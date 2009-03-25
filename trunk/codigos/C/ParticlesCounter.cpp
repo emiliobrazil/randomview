439bc061abd3c7b4db2997827fe4484eca4141dd
emilio.brazil <emilio.brazil@f8c533c6-1502-11de-99d6-5fb3f1362b5f>
2009-03-25 01:10:12 +0000

    counter pronto - so falta testar

---
+++ trunk/codigos/C/ParticlesCounter.cpp
@@ -0,0 +1,78 @@
+#include "ParticlesCounter.hpp"
+
+
+const ParticlesCounter  ParticlesCounter::operator+( const ParticlesCounter &counter ) const 
+{
+  uint32 nOp = this->_sites.size();
+  ParticlesCounter s( nOp );
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    s.set( i ,  this->_sites[i]+ counter.at(i) );
+  }
+  return s;
+}
+
+const ParticlesCounter  ParticlesCounter::operator-( const ParticlesCounter &counter ) const 
+{
+  uint32 nOp = this->_sites.size();
+  ParticlesCounter s( nOp );
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    s.set( i , this->_sites[i] - counter.at(i) );
+  }
+  return s;
+}
+
+const ParticlesCounter  ParticlesCounter::operator*( real alpha ) const
+{
+  uint32 nOp = this->_sites.size();
+  ParticlesCounter p( nOp );
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    p.set( i , this->_sites[i] * alpha );
+  }
+  return p;
+}
+
+const ParticlesCounter  ParticlesCounter::operator*( real alpha , const ParticlesCounter &counter )
+{
+  uint32 nOp = this->_sites.size();
+  ParticlesCounter p( nOp );
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    p.set( i , this->_sites[i] * alpha );
+  }
+  return p;
+}
+
+const ParticlesCounter &ParticlesCounter::operator+=( const ParticlesCounter &counter )
+{
+  uint32 nOp = this->_sites.size();
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    this->_sites[i] += counter.at(i) ;
+  }
+  return *this;
+}
+
+const ParticlesCounter &ParticlesCounter::operator-=( const ParticlesCounter &counter )
+{
+  uint32 nOp = this->_sites.size();
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    this->_sites[i] += counter.at(i) ;
+  }
+  return *this;
+}
+
+const ParticlesCounter &ParticlesCounter::operator*=( real alpha )
+{
+  uint32 nOp = this->_sites.size();
+  for( uint32 i = 0 ; i < nOp ; ++i)
+  {
+    this->_sites[i] *= alpha ;
+  }
+  return *this;
+}
+
+//  std::vector<real> _sites;
