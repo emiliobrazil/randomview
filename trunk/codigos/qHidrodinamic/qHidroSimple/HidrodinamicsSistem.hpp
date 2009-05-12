80663517687e5c5fa17dae017f9225f97e0d09ac
emilio.brazil <emilio.brazil@f8c533c6-1502-11de-99d6-5fb3f1362b5f>
2009-05-12 03:52:15 +0000

    removendo qHidrodinamic

--- trunk/codigos/qHidrodinamic/qHidroSimple/HidrodinamicsSistem.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _HIDRODINAMICSYSTEM_HPP_
-#define _HIDRODINAMICSYSTEM_HPP_
-
-
-#include "primitive_types.h"
-
-
-#endif // _HIDRODINAMICSYSTEM_HPP_
-
--- trunk/codigos/qHidrodinamic/qHidroSimple/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QtGui/QApplication>
-#include "particleshow.h"
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    ParticleShow w;
-    w.show();
-    return a.exec();
-}
