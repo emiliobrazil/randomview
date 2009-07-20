1990c63e27d2f343608270e20f8befe655188e5b
juliodaniel <juliodaniel@f8c533c6-1502-11de-99d6-5fb3f1362b5f>
2009-07-20 21:12:31 +0000

    Agora estamos usando o ranlib.c da Netlib.

--- trunk/codigos/QtRandon/cornerPercolation/random_functions.cpp
+++ trunk/codigos/QtRandon/cornerPercolation/random_functions.cpp
@@ -3,22 +3,24 @@
 #include <stdlib.h>
 
 #include "random_functions.h"
-
 #include "primitive_const.h"
 
 void setSeed( unsigned int seed )
 {
-	srand( seed ) ;
+    setall( seed, seed );
+    //srand( seed ) ;
 }
 
 real uniform01( void )
 {
-	return (real)rand()/(real)RAND_MAX;
+    return ranf();
+    //return (real)rand()/(real)RAND_MAX;
 }
 
 uint32 uniform1max( uint32 max )
 {
-	return (rand() % max) + 1 ;
+        //return (rand() % max) + 1 ;
+    return ( ignlgi() % max ) + 1; // Cuidado com a operacao %: valor retornado nao eh mais uniforme.
 }
 
 real uniformAB( real A , real B )
